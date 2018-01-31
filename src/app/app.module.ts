34d8f553db546192ef043130bbce76bf74b3bafc
Sahib Singh <sdsahib95@gmail.com>
2018-01-31 20:13:46 +0530

    radio done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    AppRoutingModule,
     AppRoutingModule
-  ],
+    
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
