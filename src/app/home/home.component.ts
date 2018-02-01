5cc6c840ef1f1a972637a40b1bb6ec177a8f514b
Sahib Singh <sdsahib95@gmail.com>
2018-02-01 19:41:17 +0530

    01-02

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  languages = ['english','hindi','french'];
+  firstName: string = "Sahib";
+  lastName: string;
   constructor() { }
 
+  firstNameChanged(value: string){
+    if (value.length > 0)
+      this.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    else
+      this.firstName = value;
+
+  }
   ngOnInit() {
   }
 
