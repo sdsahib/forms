187282a38f21262e4aeba0ddf3c8ad7ead060c0d
Sahib Singh <sdsahib95@gmail.com>
2018-02-08 18:46:34 +0530

    posting_to_server

--- src/app/services/form-poster.service.ts
+++ src/app/services/form-poster.service.ts
@@ -2,17 +2,34 @@ import { Injectable } from "@angular/core";
 import {Http} from "@angular/http";
 import { Employee } from "../models/employee.model";
 import { Observable } from "rxjs/Observable";
+import { RequestOptions,Headers,Response } from "@angular/http";
+import 'rxjs/Rx'
 
 
 @Injectable()
 export class FormPoster{
-
+     
     constructor(private http:Http){
 
     }
 
+    private extractData(res: Response){
+        let body = res.json();
+        return body.fields || {};
+    }
+
+    private handleError(error: any){
+        console.error('post error ', error);
+        return Observable.throw(error.statusText);
+    }
+
     postEmployee(employee: Employee): Observable<any>{
-        console.log("Posting Employee : ", employee);
-        return;
+        let body = JSON.stringify(employee);
+        let headers = new Headers({'Content-Type':'application/json'});
+        let options =new RequestOptions({ headers : headers});
+        //console.log("Posting Employee : ", employee);
+        return this.http.post('http://localhost:3100/postemployee',body,options)
+                .map(this.extractData)
+                .catch(this.handleError);
     }
 }
